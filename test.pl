581a78c7c8e3f9669275da683a77efaa37b41106
Jean Connelly <jconnelly@cfa.harvard.edu>
2014-09-09 11:23:43 -0400

    Imported sources at version 0.01

---
+++ test.pl
@@ -0,0 +1,30 @@
+# Before `make install' is performed this script should be runnable with
+# `make test'. After `make install' it should work as `perl test.pl'
+
+######################### We start with some black magic to print on failure.
+
+# Change 1..1 below to 1..last_test_to_print .
+# (It may become useful if the test is moved to ./t subdirectory.)
+
+BEGIN { $| = 1; print "1..1\n"; }
+END {print "not ok 1\n" unless $loaded;}
+use Ska::Convert qw(:all);
+$loaded = 1;
+print "ok 1\n";
+
+######################### End of black magic.
+
+# Insert your test code below (better if it prints "ok 13"
+# (correspondingly "not ok 13") depending on the success of chunk 13
+# of the test code):
+
+$test = 2;
+
+print "$test:  dec2hms\n";
+($rah, $dms) = dec2hms(123.456, 54.321);
+$ok = ($rah eq '8:13:49.440' && $dms eq '+54:19:15.60');
+print $ok ? "ok" : "not ok", " ", $test++, "\n";
+
+print "$test:  time2date\n";
+$ok = (time2date(12345678.9) eq '1998:143:21:20:15.716');
+print $ok ? "ok" : "not ok", " ", $test++, "\n";
